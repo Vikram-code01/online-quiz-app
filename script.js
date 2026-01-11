18f2f5797c21772a899289fc02dc4cde7ba780dd
Vikram-code01 <vk9179939@gmail.com>
2026-01-11 14:33:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function startQuiz() {
+ function startQuiz() {
   document.body.style.backgroundColor = "lightgreen";
-
-  document.getElementById("result").innerHTML =
-    "<h2>Quiz Started Successfully!</h2>";
-
   document.getElementById("startBtn").disabled = true;
+
+  document.getElementById("quiz").innerHTML = `
+    <h2>Q1. HTML stands for?</h2>
+
+    <button onclick="answer(true)">Hyper Text Markup Language</button><br><br>
+    <button onclick="answer(false)">High Text Machine Language</button><br><br>
+    <button onclick="answer(false)">Hyperlinks Text Mark Language</button><br><br>
+    <button onclick="answer(false)">None of these</button>
+  `;
+}
+
+function answer(correct) {
+  if (correct) {
+    document.getElementById("result").innerHTML =
+      "<h2>Correct Answer ✅</h2>";
+  } else {
+    document.getElementById("result").innerHTML =
+      "<h2>Wrong Answer ❌</h2>";
+  }
 }
