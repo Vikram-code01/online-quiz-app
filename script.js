592e4117182b618027d22e5ad04f117ff15e5e05
Vikram-code01 <vk9179939@gmail.com>
2026-01-12 09:36:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,77 @@
- function startQuiz() {
-  document.body.style.backgroundColor = "lightgreen";
-  document.getElementById("startBtn").disabled = true;
+// QUESTIONS
+const questions = [
+  {
+    q: "2 + 2 = ?",
+    o: ["3", "4", "5"],
+    a: 1
+  },
+  {
+    q: "Capital of India?",
+    o: ["Delhi", "Mumbai", "Patna"],
+    a: 0
+  },
+  {
+    q: "HTML stands for?",
+    o: ["Hyper Text Markup Language","huper text mark of language", "High Text Machine"],
+    a: 0
+  }
+];
 
-  document.getElementById("quiz").innerHTML = `
-    <h2>Q1. HTML stands for?</h2>
+let index = 0;
+let score = 0;
 
-    <button onclick="answer(true)">Hyper Text Markup Language</button><br><br>
-    <button onclick="answer(false)">High Text Machine Language</button><br><br>
-    <button onclick="answer(false)">Hyperlinks Text Mark Language</button><br><br>
-    <button onclick="answer(false)">None of these</button>
-  `;
+// START QUIZ
+function startQuiz() {
+  document.getElementById("startBox").style.display = "none";
+  document.getElementById("quizBox").style.display = "block";
+  index = 0;
+  score = 0;
+  loadQuestion();
 }
 
-function answer(correct) {
-  if (correct) {
-    document.getElementById("result").innerHTML =
-      "<h2>Correct Answer ✅</h2>";
-  } else {
-    document.getElementById("result").innerHTML =
-      "<h2>Wrong Answer ❌</h2>";
+// LOAD QUESTION
+function loadQuestion() {
+  document.getElementById("question").innerText = questions[index].q;
+
+  let html = "";
+  questions[index].o.forEach((opt, i) => {
+    html += `
+      <label>
+        <input type="radio" name="opt" value="${i}"> ${opt}
+      </label>
+    `;
+  });
+
+  document.getElementById("options").innerHTML = html;
+}
+
+// NEXT QUESTION
+function nextQuestion() {
+  let selected = document.querySelector('input[name="opt"]:checked');
+
+  if (!selected) {
+    alert("Select an option");
+    return;
   }
+
+  if (parseInt(selected.value) === questions[index].a) {
+    score++;
+  }
+
+  index++;
+
+  if (index < questions.length) {
+    loadQuestion();
+  } else {
+    document.getElementById("quizBox").style.display = "none";
+    document.getElementById("resultBox").style.display = "block";
+    document.getElementById("result").innerText =
+      "Score: " + score + " / " + questions.length;
+  }
+}
+
+// RESTART
+function restartQuiz() {
+  document.getElementById("resultBox").style.display = "none";
+  document.getElementById("startBox").style.display = "block";
 }
